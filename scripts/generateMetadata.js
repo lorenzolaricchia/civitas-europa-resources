8934b1b3636e23a9ec66f6c91c04d9ca3dd6c7dc
Lorenzo Laricchia <lorenzo.laricchia@outlook.it>
2024-04-03 08:51:28 +0200

    Upgraded generateMetadata parsing process (now it combines posts and authors json generation)

---
+++ scripts/generateMetadata.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+const contentsDirectory = path.join(process.cwd(), 'contents');
+const authorsMdxDirectory = path.join(contentsDirectory, '/authors');
+const postsMdxDirectory = path.join(contentsDirectory, '/posts');
+
+const metadataDirectory = path.join(process.cwd(), 'metadata');
+const authorsMetadataPath = path.join(metadataDirectory, '/authors.json');
+const postsMetadataPath = path.join(metadataDirectory, '/posts.json');
+
+const parseMetadata = (directory) => {
+    const files = fs.readdirSync(directory);
+    return files.map((filename) => {
+        const filePath = path.join(directory, filename);
+        const fileContents = fs.readFileSync(filePath, 'utf8');
+        const { data: metadata } = matter(fileContents);
+        const slug = filename.replace(/\.mdx$/, '')
+
+        return { ...metadata, slug};
+    });
+}
+
+const authorsMetadata = parseMetadata(authorsMdxDirectory);
+fs.writeFileSync(authorsMetadataPath, JSON.stringify(authorsMetadata, null, 2));
+
+const postsMetadata = parseMetadata(postsMdxDirectory);
+fs.writeFileSync(postsMetadataPath, JSON.stringify(postsMetadata, null, 2));
